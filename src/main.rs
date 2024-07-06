bcbf696c4d4f9acecbe00a7afec89eb6927022cc
Molina-Q <quentinmolina1@gmail.com>
2024-07-06 18:57:07 +0200

    Added render and draw

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{error::Error, io::{self, stdout}, time::Duration};
-
+use std::{error::Error, io, sync::mpsc, thread, time::Duration};
+use rust_invaders::{frame::new_frame, render};
 use crossterm::{cursor::{Hide, Show}, event::{self, Event, KeyCode}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
+use rust_invaders::frame;
 use rusty_audio::Audio;
 
 fn main() -> Result <(), Box<dyn Error>> {
@@ -20,9 +21,29 @@ fn main() -> Result <(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // render loop in a separete thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
     // Game Loop
     'gameloop: loop {
-        //INput
+        // Per-frame initalization
+        let curr_frame = new_frame();
+
+
+        // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
@@ -34,9 +55,15 @@ fn main() -> Result <(), Box<dyn Error>> {
                 }
             }
         }
+
+        //Draw & Render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));    
     }
 
     // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
